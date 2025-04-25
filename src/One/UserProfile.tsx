7f443d198324165e2398bb83ac437264519f5d6b
PhuongNguyen <phuongnguyen@dayoneteams.com>
2025-04-25 11:06:49 +0700

    user Profile

---
+++ src/One/UserProfile.tsx
@@ -0,0 +1,82 @@
+export const UserProfile = () => {
+  return (
+    <div className="flex w-[36rem] rounded-lg bg-white p-12">
+      <img
+        src="https://placewaifu.com/image/200"
+        className="w-28 self-start rounded-full border-[11px] border-[#e6effa]"
+      ></img>
+      <div className="m-4 space-y-7">
+        <div>
+          <h1 className="text-blue-primary text-2xl font-bold">David Grant</h1>
+          <h2 className="text-blue-primary text-[16px] font-medium">
+            3D Artist
+          </h2>
+        </div>
+
+        <div>
+          <span className="text-blue-primary mb-2 flex gap-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
+              />
+            </svg>
+            <p className="text-[16px]">4.7 Rating </p>
+          </span>
+
+          <span className="text-blue-primary mb-2 flex gap-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M20.25 8.511c.884.284 1.5 1.128 1.5 2.097v4.286c0 1.136-.847 2.1-1.98 2.193-.34.027-.68.052-1.02.072v3.091l-3-3c-1.354 0-2.694-.055-4.02-.163a2.115 2.115 0 0 1-.825-.242m9.345-8.334a2.126 2.126 0 0 0-.476-.095 48.64 48.64 0 0 0-8.048 0c-1.131.094-1.976 1.057-1.976 2.192v4.286c0 .837.46 1.58 1.155 1.951m9.345-8.334V6.637c0-1.621-1.152-3.026-2.76-3.235A48.455 48.455 0 0 0 11.25 3c-2.115 0-4.198.137-6.24.402-1.608.209-2.76 1.614-2.76 3.235v6.226c0 1.621 1.152 3.026 2.76 3.235.577.075 1.157.14 1.74.194V21l4.155-4.155"
+              />
+            </svg>
+            <p className="text-[16px]">4,447 Reviews</p>
+          </span>
+          <span className="text-blue-primary mb-2 flex gap-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
+              />
+            </svg>
+            <p className="text-[16px]">478 Students</p>
+          </span>
+        </div>
+
+        <p className="text-blue-primary text-[18px]">
+          David Grant has been making video games for a living for more than 14
+          years as a Designer, Producer, Creative Director, and Executive
+          Producer, creating games for console, mobile, PC and Facebook.
+        </p>
+        <button className="text-blue-primary h-10 w-32 rounded-sm border-2">
+          Show More
+        </button>
+      </div>
+    </div>
+  );
+};
